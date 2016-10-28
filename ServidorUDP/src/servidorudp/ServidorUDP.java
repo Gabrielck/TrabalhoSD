782a76758a4306b7141745bc57c16a7460786744
mmatheuslyra <mmatheuslyra@gmail.com>
2016-10-28 14:10:37 -0200

    Codigo inicial do servidor UDP

---
+++ ServidorUDP/src/servidorudp/ServidorUDP.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorudp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import servidorudp.Socket;
+
+/**
+ *
+ * @author Matheus Lyra
+ */
+public class ServidorUDP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+    
+    Socket socket = new Socket();
+    socket.AbreSocket(2006);
+    }
+    
+}
