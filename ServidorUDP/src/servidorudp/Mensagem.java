3e9160710052ac6fbde26d7e2242f5f0bc4660c7
mmatheuslyra <mmatheuslyra@gmail.com>
2016-10-28 14:27:25 -0200

    ServidorUDP

---
+++ ServidorUDP/src/servidorudp/Mensagem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorudp;
+
+/**
+ *
+ * @author Matheus Lyra
+ */
+public class Mensagem{
+    
+    private String partMessage;
+    private int index;
+    
+    
+    
+    
+    
+    
+    
+    
+}
