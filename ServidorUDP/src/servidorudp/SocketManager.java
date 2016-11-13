8bc1bf2042514caf639565e9b3bdfae3a62a9fbb
mmatheuslyra <mmatheuslyra@gmail.com>
2016-11-12 23:52:01 -0200

    Ajuste case 5.

--- ServidorUDP/src/servidorudp/SocketManager.java
+++ ServidorUDP/src/servidorudp/SocketManager.java
@@ -120,6 +120,7 @@ public class SocketManager {
                         if(frases == null)
                             SendMessage(socket, packet, "0#0");
                         else{
+                            SendMessage(socket, packet, "0#" + frases.length);
                             for(int i = 0; i < frases.length; i++){
                                 System.out.println("String: " + frases[i].getFrase());
                                 EnviarMensagemParticionada(socket, packet, frases[i].getFrase(), i);  
